bce60664c5fbc5a42fd94eda915d5250124a4e02
Metalheart07 <cris7darkline@gmail.com>
2012-05-06 18:45:27 -0500

    Aqui esta el cascaron del proyecto

---
+++ src/SistemaExperto/SistemaExpertoTDAHI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SistemaExperto;
+
+/**
+ *
+ * @author Megadeth
+ */
+public class SistemaExpertoTDAHI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char entrada='0';
+        motor m=new motor();
+        entrada=m.leer();
+        System.out.println("Cadena: "+entrada);
+         
+        // TODO code application logic here
+    }
+}
---
+++ src/SistemaExperto/motor.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SistemaExperto;
+
+import java.io.*;
+
+/**
+ *
+ * @author Megadeth
+ */
+public class motor {
+    char leer(){
+    char cadena[];
+    cadena=new char[13];
+      File archivo = null;
+      FileReader fr = null;
+      BufferedReader br = null;
+      try {
+         // Apertura del fichero y creacion de BufferedReader para poder
+         // hacer una lectura comoda (disponer del metodo readLine()).
+         archivo = new File ("entradas.txt");
+         fr = new FileReader (archivo);
+         br = new BufferedReader(fr);
+         // Lectura del fichero
+         String linea;
+         while((linea=br.readLine())!=null)
+             for(int i=0;i<14;i++)
+             {
+                 String lineachar=String.valueOf(linea);
+                 System.out.println("lieneachar "+lineachar.charAt(i));
+                 cadena[i]=lineachar.charAt(i);
+                 System.out.println("cadena arreglo "+cadena[i]);
+                 
+             }
+            System.out.println(linea);
+      }
+      catch(Exception e1){
+         e1.printStackTrace();
+      }finally{
+         // En el finally cerramos el fichero, para asegurarnos
+         // que se cierra tanto si todo va bien como si salta 
+         // una excepcion.
+         try{                    
+            if( null != fr ){   
+               fr.close();     
+            }                  
+         }catch (Exception e2){ 
+            e2.printStackTrace();
+         }
+      }
+      return cadena[0];//en el arreglo "cadena" estan las entradas
+   }
+    
+}
